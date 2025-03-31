5e79e7ebfea6baa314465a167d0d77f4c3e24419
MarcellNicson <marcellnicson@gmail.com>
2025-03-31 17:31:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const TELEGRAM_BOT_TOKEN = "7380899592:AAHIo1pydLaKkaeT7k7h5S0pM1GNV-_rrDc"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "-4721214951"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_CHAT_ID = "4721214951"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
